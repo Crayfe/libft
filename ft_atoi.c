cceea6491edfec41aab4bea3b491dfe7b970047c
Cristian Ayuso <crayfe@gmail.com>
2024-09-26 10:21:19 +0200

    Actualizar ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -11,15 +11,30 @@
 /* ************************************************************************** */
 #include "libft.h"
 
+int aux_is_space(char c)
+{
+if (c == ' ' || c == '\n' || c = '\f' || c = '\v' || c = '\r')
+return (1);
+return (0);
+}
+int	aux_clear_spaces(const char *s)
+{
+int i;
+
+i = 0;
+while (aux_is_space(s[i]))
+++i;
+return (i);
+}
 int	ft_atoi(const char *nptr)
 {
 	int	i;
 	int	sign;
 	int	numb;
 
-	i = 0;
 	sign = 1;
 	numb = 0;
+i = aux_clear_spaces();
 	if (nptr[0] == '-' || nptr[0] == '+')
 	{
 		if (nptr[0] == '-')
