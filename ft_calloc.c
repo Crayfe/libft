5a49fd419836f286c5b5651f973f66ae91367cfd
Cristian Ayuso <crayfe@gmail.com>
2024-09-27 16:47:50 +0200

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -16,10 +16,13 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*ptr;
 
 	if (nmemb == 0 || size == 0)
-		return (0);
+	{
+		nmemb = 1;
+		size = 1;
+	}
 	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (0);
-	ft_bzero(ptr, nmemb);
+	ft_memset(ptr, nmemb * size);
 	return (ptr);
 }
