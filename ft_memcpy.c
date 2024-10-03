80d61d270ff88d01c25b6b69f99cfe907f755afc
Cristian Ayuso <crayfe@gmail.com>
2024-10-04 00:09:48 +0200

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cayuso-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cayuso-f <cayuso-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 15:46:10 by cayuso-f          #+#    #+#             */
 /*   Updated: 2024/09/30 11:54:18 by cayuso-f         ###   ########.fr       */
