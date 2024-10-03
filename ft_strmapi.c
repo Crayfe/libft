05ac20a3312650de609da6190ca4156346e88e08
Cristian Ayuso <crayfe@gmail.com>
2024-10-04 00:08:17 +0200

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cayuso-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cayuso-f <cayuso-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 11:30:29 by cayuso-f          #+#    #+#             */
 /*   Updated: 2024/09/26 13:56:37 by cayuso-f         ###   ########.fr       */
