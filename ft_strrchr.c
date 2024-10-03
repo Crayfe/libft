7b3e25c8bf6fe163a4555e6cd5af6d514df475b7
Cristian Ayuso <crayfe@gmail.com>
2024-10-04 00:07:03 +0200

    Update ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cayuso-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cayuso-f <cayuso-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 14:13:33 by cayuso-f          #+#    #+#             */
 /*   Updated: 2024/09/26 13:28:26 by cayuso-f         ###   ########.fr       */
