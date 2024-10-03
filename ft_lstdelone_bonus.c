99b373188d1c1ff7d812003d92bfc6f096f29055
Cristian Ayuso <crayfe@gmail.com>
2024-10-03 23:55:47 +0200

    Update ft_lstdelone_bonus.c

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cayuso-f <marvin@42.fr>                    +#+  +:+       +#+        */
+//*   By: cayuso-f <cayuso-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 13:01:51 by cayuso-f          #+#    #+#             */
 /*   Updated: 2024/09/25 15:23:46 by cayuso-f         ###   ########.fr       */
