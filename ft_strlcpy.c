0af5746f8089df5cdabb3c8045cc6b38ac5e776e
Cristian Ayuso <crayfe@gmail.com>
2024-10-04 00:06:41 +0200

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cayuso-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cayuso-f <cayuso-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 10:54:24 by cayuso-f          #+#    #+#             */
 /*   Updated: 2024/09/20 16:22:56 by cayuso-f         ###   ########.fr       */
