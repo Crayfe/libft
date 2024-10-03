87e58a89013551bd36f0c3ecaccad762086430bb
Cristian Ayuso <crayfe@gmail.com>
2024-10-03 23:51:16 +0200

    Update ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cayuso-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cayuso-f <cayuso-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 12:18:20 by cayuso-f          #+#    #+#             */
 /*   Updated: 2024/09/18 12:35:26 by cayuso-f         ###   ########.fr       */
