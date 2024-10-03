ed683cf87a9301e304d28eb40bee98fe45d9f52a
Cristian Ayuso <crayfe@gmail.com>
2024-10-04 00:07:55 +0200

    Update ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cayuso-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cayuso-f <cayuso-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 15:29:36 by cayuso-f          #+#    #+#             */
 /*   Updated: 2024/09/17 15:35:20 by cayuso-f         ###   ########.fr       */
